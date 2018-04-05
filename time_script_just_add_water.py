1afbb4b27f5d8581a173ba8ddaf7b1deec257e58
Jake <jakemurph996@gmail.com>
2018-04-05 13:33:40 +0100

    Courtesy of @phelantomas

--- time_script_just_add_water.py
+++ time_script_just_add_water.py
@@ -1,6 +1,4 @@
 import os
+import urllib2
 
-todays_date="20180405"
-todays_time="12:48:00"
-
-os.system("for I in 1 2 3 4; do ssh pi@p$I.local sudo date +%Y%m%d --set='$todays_date'; ssh pi@p$I.local sudo date +%H%M%S --set='$todays_time'; done")
+os.system("for I in 1 2 3 4; do ssh pi@p$I.local sudo date +%Y%m%d --set='$todays_date'; ssh pi@p$I.local sudo date +%H%M%S -s 12:48:00; done")
